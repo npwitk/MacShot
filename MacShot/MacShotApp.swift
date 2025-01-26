338b91e3f8120581ac8f7b7f0385a6352041a07b
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-01-26 14:01:47 +0700

    Initial Commit

---
+++ MacShot/MacShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacShotApp.swift
+//  MacShot
+//
+//  Created by Nonprawich I. on 26/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MacShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
